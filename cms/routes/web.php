3adbb2c081db820d2d5cf45d17eca1a407b738ce
EC2 Default User <ec2-user@ip-172-31-40-178.ec2.internal>
2022-08-04 11:49:42 +0000

    1回目

--- cms/routes/web.php
+++ cms/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TeamsController;//追記
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [TeamsController::class, 'index']);
+Route::post('teams', [TeamsController::class, 'store']);
+Route::get('team/{team_id}', [TeamsController::class, 'join']);
+Route::get('teamedit/{team}', [TeamsController::class, 'edit']); 
+//チーム更新処理
+Route::post('teams/update',  [TeamsController::class, 'update']);
+// チーム詳細表示
+Route::get('teams/{team}', [TeamsController::class, 'show']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
